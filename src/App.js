0097045450e8a5dfe0bd37783eeb74a2a275e0ab
Abdelghani issouani <abdelghaniissouani@gmail.com>
2022-01-25 17:08:21 +0100

    update histogramme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import About from "./Component/About/About";
 import Home from "./Component/Home/Home";
 import Login from "./Component/Login/Login";
+import Histo from "./Component/HistogramPage/Histo"
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/histogramme" element={<Histo />} />
+        
       </Routes>
     </>
   );
